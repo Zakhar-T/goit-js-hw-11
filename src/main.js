ef361fc804de658e522a479e947b99ffea1125b8
Zakhar-T <zakhar.null@gmail.com>
2025-03-14 22:58:12 +0200

    set basic structure

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import "simplelightbox/dist/simple-lightbox.min.css";
