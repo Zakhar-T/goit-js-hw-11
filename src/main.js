e7b93a8700df47c18f2251a3bbb18c389e8aa583
Zakhar-T <zakhar.null@gmail.com>
2025-03-18 07:44:05 +0200

    edit task 1

--- src/main.js
+++ src/main.js
@@ -4,13 +4,6 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const notificationInstance = iziToast.error({
-    message: "Sorry, there are no images matching your search query. Please try again!",
-    position: "topRight",
-});
-
-
-
 axios.defaults.baseURL = "https://pixabay.com/api/";
 
 const form = document.querySelector(".form");
@@ -24,22 +17,25 @@ const searchParams = {
     safesearch: true,
 };
 
-form.addEventListener("submit", showPhotos);
+form.addEventListener("submit", renderPhotos);
+gallery.addEventListener("click", showPhoto)
 
-function showPhotos(event) {
+function renderPhotos(event) {
     event.preventDefault();
     gallery.innerHTML = "";
 
-    const searchText = event.currentTarget.elements.search.value;
+    const searchText = event.currentTarget.elements.search.value.trim();
     searchParams.q = searchText;
-    fetchPhotos()
+    fetchPhotos();
 }
 
 function fetchPhotos() {
     return axios.get(`?${new URLSearchParams(searchParams)}`)
         .then(response => {
-            if (response.status !== 200) {
-                return notificationInstance;
+            console.log(response);
+            if (response.data.totalHits === 0) {
+                showNotification();
+                return
             }
             return createMarkup(response.data.hits) })
         .catch(error => console.log(error));
@@ -47,15 +43,35 @@ function fetchPhotos() {
 
 function createMarkup(images) {
     const markup = images.map(image => {
+        const { webformatURL, tags, largeImageURL, likes, views, comments, downloads } = image;
         return `<li class="gallery-card">
-          <img src="${image.webformatURL}" alt="${image.id}" class="card-image" width="360" height="152" />
-          <ul class="card-caption">
-            <li class="image-tag"><p>Likes<br />${image.likes}</p></li>
-            <li class="image-tag"><p>Views<br />${image.views}</p></li>
-            <li class="image-tag"><p>Comments<br />${image.comments}</p></li>
-            <li class="image-tag"><p>Downloads<br />${image.downloads}</p></li>
-          </ul>
+          <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags.split(",")[0]}" class="card-image" width="360" height="152" />
+            <ul class="card-caption">
+              <li class="image-tag"><p>Likes<br />${likes}</p></li>
+              <li class="image-tag"><p>Views<br />${views}</p></li>
+              <li class="image-tag"><p>Comments<br />${comments}</p></li>
+              <li class="image-tag"><p>Downloads<br />${downloads}</p></li>
+            </ul>
+          </a>
         </li>`;
     }).join("");
     gallery.insertAdjacentHTML("beforeend", markup);
+}
+
+function showNotification() {
+    iziToast.error({
+        message: "Sorry, there are no images matching your search query. Please try again!",
+        position: "topRight",
+    });
+}
+
+function showPhoto(event) {
+    if (event.target.nodeName === "A") {
+        event.preventDefault();
+        new SimpleLightbox('.gallery li a', {
+            captionsData: "alt",
+            captionDelay: 250,
+        });
+    }
 }
